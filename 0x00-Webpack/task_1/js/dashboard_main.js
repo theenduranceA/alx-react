404619e10a8e6e6cebff50b57e42252d7052416d
theenduranceA <theenduranceaneke@gmail.com>
2024-01-29 19:25:06 +0000

    Updated

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,18 @@
 const $ = require('jquery');
 const _ = require('lodash');
 
+
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+$('body').append("<p id='count'></p>");
+$('body').append('Copyright - Holberton School');
 
-let count = 0;
 
-function updateCounter() { count++; }
-
-$('button').click(_.debounce(() => {
-  updateCounter();
+const updateCounter = _.debounce(() => {
+  let count = parseInt($('#count').text().replace(' clicks on the button', '')) || 0;
+  count++;
   $('#count').text(`${count} clicks on the button`);
-}));
+}, 500, {loading: true, trailing: false});
+
+$('button').on('click', updateCounter);
