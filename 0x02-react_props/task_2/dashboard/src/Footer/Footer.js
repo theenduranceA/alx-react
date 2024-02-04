1babb3b04bd51ac7f579665ed8ce827b44d2d44c
theenduranceA <theenduranceaneke@gmail.com>
2024-02-04 01:24:37 +0100

    Split the Notifications component

---
+++ 0x02-react_props/task_2/dashboard/src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { getFullYear, getFooterCopy } from './utils';
+import './Footer.css';
+
+
+function Footer() {
+    return(
+        <div className="App-footer">
+          <p>Copyright 2020 - holberton School</p>
+        </div>
+    );
+}
+
+export default Footer;
