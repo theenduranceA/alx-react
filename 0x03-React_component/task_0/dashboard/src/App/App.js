0e8baf1f71cfaec3f4958692cdb1d3f1f0c8e183
theenduranceA <theenduranceaneke@gmail.com>
2024-02-07 17:07:50 +0100

    Updated

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -7,46 +7,45 @@ import CourseList from '../CourseList/CourseList';
 import Notifications from '../Notifications/Notifications';
 import { getLatestNotification } from '../utils/utils';
 /* Proptypes */
-import { bool } from 'prop-types';
+import propTypes from 'prop-types';
 /* Styles */
 import './App.css';
 
-const listCourses = [
-  { id: 1, name: 'ES6', credit: 60 },
-  { id: 2, name: 'Webpack', credit: 20 },
-  { id: 3, name: 'React', credit: 40 }
-];
+class App extends React.Component {
+  listCourses = [
+    { id: 1, name: "ES6", credit: 60 },
+    { id: 2, name: "Webpack", credit: 20 },
+    { id: 3, name: "React", credit: 40 },
+  ];
 
-const listNotifications = [
-  { id: 1, type: 'default', value: 'New course available' },
-  { id: 2, type: 'urgent', value: 'New resume available' },
-  { id: 3, type: 'urgent', html: { __html: getLatestNotification()} }
-];
+  listNotifications = [
+    { id: 1, type: "default", value: "New course available" },
+    { id: 2, type: "urgent", value: "New resume available" },
+    { id: 3, type: "urgent", html: getLatestNotification() },
+  ];
 
-class App extends Component {
-  render (){
+  render() {
     return (
-      <>
-        <Notifications listNotifications={listNotifications} />
+      <React.Fragment>
         <div className="App">
-          {/* Header */}
-          <Header />
-          {/* Body */}
-          {this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
-          {/* Footer */}
+          <div className="heading-section">
+            <Notifications listNotifications={this.listNotifications} />
+            <Header />
+          </div>
+          {this.props.isLoggedIn ? <CourseList listCourses={this.listCourses} /> : <Login />}
           <Footer />
         </div>
-      </>
+      </React.Fragment>
     );
   }
 }
 
-App.propTypes = {
-  isLoggedIn: bool,
-};
-
 App.defaultProps = {
   isLoggedIn: false,
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
 };
 
 export default App;
