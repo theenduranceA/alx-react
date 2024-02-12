21b8c558533d06daf5eefecafde03aedbb60bb30
theenduranceA <theenduranceaneke@gmail.com>
2024-02-12 13:00:14 +0100

    Inline styling

---
+++ 0x04-React_inline_styling/task_0/dashboard/src/App/App.js
@@ -0,0 +1,85 @@
+import React from 'react';
+/* Components */
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import CourseList from '../CourseList/CourseList';
+import Notifications from '../Notifications/Notifications';
+import BodySection from '../BodySection/BodySection';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+/* Utils */
+import { getLatestNotification } from '../utils/utils';
+/* Proptypes */
+import { bool } from 'prop-types';
+/* Styles */
+import './App.css';
+
+const listCourses = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 }
+];
+
+const listNotifications = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: { __html: getLatestNotification()} }
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  ComponentDidMount() {
+    document.addEventListener("keydown", this.handleClick);
+  }
+
+  handleClick(event) {
+    if (event.ctrlKey && event.key === 'h') {
+      event.preventDefault();
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  }
+
+  render (){
+    const { isLoggedIn } = this.props;
+    return (
+      <>
+        <Notifications listNotifications={listNotifications} />
+        <div className="App">
+          {/* Header */}
+          <Header />
+          {/* Body */}
+          { isLoggedIn ?
+            <BodySectionWithMarginBottom title='Course list'>
+              <CourseList listCourses={listCourses} />
+            </BodySectionWithMarginBottom> :
+            <BodySectionWithMarginBottom title='Log in to continue'>
+              <Login />
+            </BodySectionWithMarginBottom>
+            }
+            <BodySection title='News from the School'>
+              <p>So, here i am testing out my frontend skills. Feels good, ABSOLUTELY!!!</p>
+            </BodySection>
+          {/* Footer */}
+          <Footer />
+        </div>
+      </>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => void(0),
+};
+
+export default App;
---
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function CourseListRow(props) {
+  let cell;
+  const style = {
+    headerStyle: { backgroundColor: '#deb5b545' },
+    bodyStyle: { backgroundColor: '#f5f5f5ab' },
+  };
+  
+  if (!props.isHeader) {
+    cell = (
+      <>
+      <td>{props.textFirstCell}</td>
+      <td>{props.textSecondCell}</td>
+      </>
+    );
+  }
+  else {
+    if (props.textSecondCell) {
+      cell = (
+        <>
+        <th>{props.textFirstCell}</th>
+        <th>{props.textSecondCell}</th>
+        </>
+      );
+    }
+    else cell = (<th colSpan='2'>{props.textFirstCell}</th>);
+  }
+  return(<tr>{cell}</tr>);
+}
+
+CourseListRow.propTypes = {
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.string,
+};
+
+CourseListRow.defaultProps = {
+  isHeader: false,
+  textSecondCell: null,
+};
+
+export default CourseListRow;
