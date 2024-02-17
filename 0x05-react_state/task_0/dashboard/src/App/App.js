d9b9087c7ab330df34c7b58282b0ba0a28a30a59
theenduranceA <theenduranceaneke@gmail.com>
2024-02-17 13:39:15 +0100

    Adding a local state for notifications

---
+++ 0x05-react_state/task_0/dashboard/src/App/App.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import CourseList from '../CourseList/CourseList';
+import Notifications from '../Notifications/Notifications';
+import BodySection from '../BodySection/BodySection';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import { getLatestNotification } from '../utils/utils';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+const listCourses = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 }
+];
+
+const listNotifications = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: { __html: getLatestNotification()} }
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { displayDrawer: false };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+  }
+
+  ComponentDidMount() {
+    document.addEventListener("keydown", this.handleClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleClick);
+  }
+
+  handleClick(event) {
+    if (event.ctrlKey && event.key === 'h') {
+      event.preventDefault();
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  }
+
+  handleDisplayDrawer() {
+    this.setState({ displayDrawer: true });
+  }
+
+  handleHideDrawer() {
+    this.setState({ displayDrawer: false })
+  }
+
+  render () {
+    const { isLoggedIn } = this.props;
+    const { displayDrawer } = this.state;
+    return (
+      <React.Fragment>
+        <Notifications 
+          listNotifications={listNotifications}
+          displayDrawer={displayDrawer}
+          handleDisplayDrawer={this.handleDisplayDrawer}
+          handleHideDrawer={this.handleHideDrawer}
+        />
+        <div className="App">
+          <Header />
+          <div className={css(style.body)}>
+            {isLoggedIn ?
+              <BodySectionWithMarginBottom title='Course list'>
+                <CourseList listCourses={listCourses} />
+              </BodySectionWithMarginBottom> :
+              <BodySectionWithMarginBottom title='Log in to continue'>
+                <Login />
+              </BodySectionWithMarginBottom>}
+            <BodySection title='News from the School'>
+              <p>So, here i am testing out my frontend skills. Feels good, ABSOLUTELY!!!</p>
+            </BodySection>
+          </div>
+          <div className={css(style.footer)}>
+            <Footer></Footer>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  App: {
+    height: "100vh",
+    maxWidth: "100vw",
+    position: "relative",
+    fontFamily: "Arial, Helvetica, sans-serif",
+  },
+});
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => void(0),
+};
+
+export default App;
