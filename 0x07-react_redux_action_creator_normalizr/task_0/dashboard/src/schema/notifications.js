3f9b4d610b935661caca69a1daf810d892346659
theenduranceA <theenduranceaneke@gmail.com>
2024-02-27 15:51:22 +0100

    Read data from a JSON

---
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -0,0 +1,5 @@
+import * as notifications from "../../../../notifications.json";
+
+export default function getAllNotificationsByUser(userId) {
+  return notifications.filter((notification) => notification.author.id === userId).map((notification) => notification.context);
+}
