6535223084a23b85263e036175d99dd0eedbcdf6
theenduranceA <theenduranceaneke@gmail.com>
2024-03-11 22:57:09 +0100

    Task 3-6

---
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.App';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { Map } from "immutable";
+import uiReducer, { initialState } from "./reducers/uiReducer";
+
+const store = createStore(uiReducer, Map(initialState));
+
+ReactDOM.render(
+  <React.StrictMode>
+     <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
