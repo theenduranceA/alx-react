5c017a5d21783e28abe7b61e3d3e362bee0d2969
theenduranceA <theenduranceaneke@gmail.com>
2024-03-12 13:31:35 +0100

    Task 22

---
+++ 0x09-react_redux_connectors_and_providers/task_6/dashboard/src/reducers/rootReducer.js
@@ -0,0 +1,17 @@
+import { Map } from "immutable";
+
+import { courseReducer, initialState as initialStateCourse } from "./courseReducer";
+import { notificationReducer, initialState as initialStateNotification } from "./notificationReducer";
+import uiReducer, { initialState as initialStateUiReducer } from "./uiReducer";
+
+const rootReducer = {
+  courses: courseReducer,
+  notifications: notificationReducer,
+  ui: uiReducer,
+};
+
+export const initialState = {
+  courses: Map(initialStateCourse),
+  notifications: Map(initialStateNotification),
+  ui: Map(initialStateUiReducer),
+};
